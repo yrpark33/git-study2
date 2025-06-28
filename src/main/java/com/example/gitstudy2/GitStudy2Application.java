a55953c572accb2163101c7d4f58a695e5d329fb
yrpark33 <yrpark33@naver.com>
2025-06-28 14:06:47 +0900

    init commit

---
+++ src/main/java/com/example/gitstudy2/GitStudy2Application.java
@@ -0,0 +1,13 @@
+package com.example.gitstudy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStudy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitStudy2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitstudy2/GitStudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitstudy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudy2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
