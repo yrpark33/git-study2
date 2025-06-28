b25bc35793e71b1b059bf2c6744b5d7295f6662e
yrpark33 <yrpark33@naver.com>
2025-06-28 14:29:51 +0900

    add new line in main

--- src/main/java/com/example/gitstudy2/GitStudy2Application.java
+++ src/main/java/com/example/gitstudy2/GitStudy2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitStudy2Application {
 
     public static void main(String[] args) {
+        System.out.println("new line");
         SpringApplication.run(GitStudy2Application.class, args);
     }
 
