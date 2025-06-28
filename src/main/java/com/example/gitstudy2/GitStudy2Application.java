ddf1ac513be234552375d322d0280aed9d9a8f46
yrpark33 <yrpark33@naver.com>
2025-06-28 21:37:40 +0900

    test

--- src/main/java/com/example/gitstudy2/GitStudy2Application.java
+++ src/main/java/com/example/gitstudy2/GitStudy2Application.java
@@ -8,6 +8,9 @@ public class GitStudy2Application {
 
     public static void main(String[] args) {
         System.out.println("new line");
+        System.out.println("");
+        System.out.println("a");
+        System.out.println("b");
         SpringApplication.run(GitStudy2Application.class, args);
     }
 
---
+++ src/main/java/com/example/gitstudy2/Member.java
@@ -0,0 +1,4 @@
+package com.example.gitstudy2;
+
+public class Member {
+}
